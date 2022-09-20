d6bb4c98bc1c6105a9e3578555cff716c2714bd5
tylersortijas <tylersortijas@gmail.com>
2022-09-20 06:17:28 -0700

    completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let text = string;
+    let myArray = text.split("");
+    let reverse = myArray.reverse().join("");
 
+    return reverse;
 };
 
 // Do not edit below this line
