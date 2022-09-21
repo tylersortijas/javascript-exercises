0ce703711b3ee4c63b7cbfc2d3e90e82c7b6d468
tylersortijas <tylersortijas@gmail.com>
2022-09-21 06:18:46 -0700

    completed 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempFah) {
+  let celsius = (tempFah - 32) * .5556;
+  
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(tempCel) {
+  let fahrenheit = (tempCel * 1.8) + 32;
 
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
