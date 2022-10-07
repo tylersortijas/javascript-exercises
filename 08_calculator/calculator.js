7beb5df58658c193092d54a80afe11bde8bdec92
tylersortijas <tylersortijas@gmail.com>
2022-10-06 18:35:11 -0700

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  const add = num1 + num2;
+  return add;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const subtract = num1 - num2;
+  return subtract;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const reduce = arr.reduce((partialSum, a) => partialSum + a, 0);
+  return reduce;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multi = arr.reduce((a, b) => a * b);
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+	const powerTo = Math.pow(num, pwr);
+  return powerTo;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0) return 1;
+  let product = 1;
+  for(let i = n; i > 0; i--){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
